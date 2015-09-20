9594f81a9e3aed41799612591182a424148d7b10
Matt Bierner <mattbierner@gmail.com>
2015-09-20 14:09:16 -0700

    Removed some unneeded static qualifiers

--- template assembly/register.h
+++ template assembly/register.h
@@ -19,63 +19,63 @@ struct GeneralPurposeRegister {
 /**
     One byte GP registers.
 */
-static constexpr auto al = GeneralPurposeRegister<1, 0>{};
-static constexpr auto cl = GeneralPurposeRegister<1, 1>{};
-static constexpr auto dl = GeneralPurposeRegister<1, 2>{};
-static constexpr auto bl = GeneralPurposeRegister<1, 3>{};
-static constexpr auto ah = GeneralPurposeRegister<1, 4>{};
-static constexpr auto ch = GeneralPurposeRegister<1, 5>{};
-static constexpr auto dh = GeneralPurposeRegister<1, 6>{};
-static constexpr auto bh = GeneralPurposeRegister<1, 7>{};
+constexpr auto al = GeneralPurposeRegister<1, 0>{};
+constexpr auto cl = GeneralPurposeRegister<1, 1>{};
+constexpr auto dl = GeneralPurposeRegister<1, 2>{};
+constexpr auto bl = GeneralPurposeRegister<1, 3>{};
+constexpr auto ah = GeneralPurposeRegister<1, 4>{};
+constexpr auto ch = GeneralPurposeRegister<1, 5>{};
+constexpr auto dh = GeneralPurposeRegister<1, 6>{};
+constexpr auto bh = GeneralPurposeRegister<1, 7>{};
 
 /**
     Two byte GP registers.
 */
-static constexpr auto ax = GeneralPurposeRegister<2, 0>{};
-static constexpr auto cx = GeneralPurposeRegister<2, 1>{};
-static constexpr auto dx = GeneralPurposeRegister<2, 2>{};
-static constexpr auto bx = GeneralPurposeRegister<2, 3>{};
-static constexpr auto sp = GeneralPurposeRegister<2, 4>{};
-static constexpr auto bp = GeneralPurposeRegister<2, 5>{};
-static constexpr auto si = GeneralPurposeRegister<2, 6>{};
-static constexpr auto di = GeneralPurposeRegister<2, 7>{};
+constexpr auto ax = GeneralPurposeRegister<2, 0>{};
+constexpr auto cx = GeneralPurposeRegister<2, 1>{};
+constexpr auto dx = GeneralPurposeRegister<2, 2>{};
+constexpr auto bx = GeneralPurposeRegister<2, 3>{};
+constexpr auto sp = GeneralPurposeRegister<2, 4>{};
+constexpr auto bp = GeneralPurposeRegister<2, 5>{};
+constexpr auto si = GeneralPurposeRegister<2, 6>{};
+constexpr auto di = GeneralPurposeRegister<2, 7>{};
 
 /**
     Four byte GP registers.
 */
-static constexpr auto eax = GeneralPurposeRegister<4, 0>{};
-static constexpr auto ecx = GeneralPurposeRegister<4, 1>{};
-static constexpr auto edx = GeneralPurposeRegister<4, 2>{};
-static constexpr auto ebx = GeneralPurposeRegister<4, 3>{};
-static constexpr auto esp = GeneralPurposeRegister<4, 4>{};
-static constexpr auto ebp = GeneralPurposeRegister<4, 5>{};
-static constexpr auto esi = GeneralPurposeRegister<4, 6>{};
-static constexpr auto edi = GeneralPurposeRegister<4, 7>{};
+constexpr auto eax = GeneralPurposeRegister<4, 0>{};
+constexpr auto ecx = GeneralPurposeRegister<4, 1>{};
+constexpr auto edx = GeneralPurposeRegister<4, 2>{};
+constexpr auto ebx = GeneralPurposeRegister<4, 3>{};
+constexpr auto esp = GeneralPurposeRegister<4, 4>{};
+constexpr auto ebp = GeneralPurposeRegister<4, 5>{};
+constexpr auto esi = GeneralPurposeRegister<4, 6>{};
+constexpr auto edi = GeneralPurposeRegister<4, 7>{};
 
 /**
     Eight byte GP registers.
 */
-static constexpr auto rax = GeneralPurposeRegister<8, 0>{};
-static constexpr auto rcx = GeneralPurposeRegister<8, 1>{};
-static constexpr auto rdx = GeneralPurposeRegister<8, 2>{};
-static constexpr auto rbx = GeneralPurposeRegister<8, 3>{};
-static constexpr auto rsp = GeneralPurposeRegister<8, 4>{};
-static constexpr auto rbp = GeneralPurposeRegister<8, 5>{};
-static constexpr auto rsi = GeneralPurposeRegister<8, 6>{};
-static constexpr auto rdi = GeneralPurposeRegister<8, 7>{};
-static constexpr auto r0 = rax;
-static constexpr auto r1 = rcx;
-static constexpr auto r2 = rdx;
-static constexpr auto r3 = rbx;
-static constexpr auto r4 = rsp;
-static constexpr auto r5 = rbp;
-static constexpr auto r6 = rsi;
-static constexpr auto r7 = rdi;
-static constexpr auto r8 = GeneralPurposeRegister<8, 8>{};
-static constexpr auto r9 = GeneralPurposeRegister<8, 9>{};
-static constexpr auto r10 = GeneralPurposeRegister<8, 10>{};
-static constexpr auto r11 = GeneralPurposeRegister<8, 11>{};
-static constexpr auto r12 = GeneralPurposeRegister<8, 12>{};
-static constexpr auto r13 = GeneralPurposeRegister<8, 13>{};
-static constexpr auto r14 = GeneralPurposeRegister<8, 14>{};
-static constexpr auto r15 = GeneralPurposeRegister<8, 15>{};
+constexpr auto rax = GeneralPurposeRegister<8, 0>{};
+constexpr auto rcx = GeneralPurposeRegister<8, 1>{};
+constexpr auto rdx = GeneralPurposeRegister<8, 2>{};
+constexpr auto rbx = GeneralPurposeRegister<8, 3>{};
+constexpr auto rsp = GeneralPurposeRegister<8, 4>{};
+constexpr auto rbp = GeneralPurposeRegister<8, 5>{};
+constexpr auto rsi = GeneralPurposeRegister<8, 6>{};
+constexpr auto rdi = GeneralPurposeRegister<8, 7>{};
+constexpr auto r0 = rax;
+constexpr auto r1 = rcx;
+constexpr auto r2 = rdx;
+constexpr auto r3 = rbx;
+constexpr auto r4 = rsp;
+constexpr auto r5 = rbp;
+constexpr auto r6 = rsi;
+constexpr auto r7 = rdi;
+constexpr auto r8 = GeneralPurposeRegister<8, 8>{};
+constexpr auto r9 = GeneralPurposeRegister<8, 9>{};
+constexpr auto r10 = GeneralPurposeRegister<8, 10>{};
+constexpr auto r11 = GeneralPurposeRegister<8, 11>{};
+constexpr auto r12 = GeneralPurposeRegister<8, 12>{};
+constexpr auto r13 = GeneralPurposeRegister<8, 13>{};
+constexpr auto r14 = GeneralPurposeRegister<8, 14>{};
+constexpr auto r15 = GeneralPurposeRegister<8, 15>{};
