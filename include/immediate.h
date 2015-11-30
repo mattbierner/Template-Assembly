39d6456f41f5a68f01e2e82522edd64d80ac4bb9
Hugues Morisset <morisset.hugues@gmail.com>
2015-11-29 23:00:24 -0800

    Move immediate compile time tests into test folder

--- include/immediate.h
+++ include/immediate.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <type_traits>
 #include <stdexcept>
 #include <utility>
 
@@ -154,22 +153,4 @@ using qword = Immediate<int64_t, x>;
 template <char... values>
 constexpr auto operator ""_q() {
     return typename Details::ImmediateFromString<typename qword<0>::type, values...>::type{};
-}
-
-namespace {
-constexpr auto test_1_decimal = std::is_same<byte<4>, decltype(4_b)>();
-constexpr auto test_2_decimal = std::is_same<byte<42>, decltype(42_b)>();
-constexpr auto test_neg1_decimal = std::is_same<byte<-4>, decltype(-4_b)>();
-constexpr auto test_clamp = std::is_same<byte<-127>, decltype(129_b)>();
-
-constexpr auto test_hex = std::is_same<byte<4>, decltype(0x4_b)>();
-constexpr auto test_hex_digit = std::is_same<byte<10>, decltype(0xa_b)>();
-constexpr auto test_hex_digit_uppser = std::is_same<byte<10>, decltype(0xA_b)>();
-constexpr auto test_multi_hex_digit = std::is_same<byte<95>, decltype(0x5f_b)>();
-
-constexpr auto test_octal_single = std::is_same<byte<4>, decltype(04_b)>();
-constexpr auto test_octal_multi = std::is_same<byte<39>, decltype(047_b)>();
-
-constexpr auto fdas = std::is_same<byte<12>, decltype(1'2_b)>();
-
 }
