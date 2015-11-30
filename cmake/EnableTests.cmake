b1959a454fa392965ae5772bc3d9c724276f4c9d
Hugues Morisset <morisset.hugues@gmail.com>
2015-11-30 11:10:10 -0800

    Generate test target function

--- cmake/EnableTests.cmake
+++ cmake/EnableTests.cmake
@@ -1,11 +1,18 @@
-cmake_minimum_required(VERSION 3.0.0)
+cmake_minimum_required(VERSION 3.0.2)
 
-# Add test target
-# --- enable unit tests if desired ---
-if(BUILD_TESTS)
+include(CMakeParseArguments)
+
+# GENERATE_TEST_TARGET(COVERAGE;VALGRIND;TARGET_NAME;FILES)
+FUNCTION(GENERATE_TEST_TARGET)
+
+  cmake_parse_arguments(ARG "" "COVERAGE;VALGRIND;TARGET_NAME" "FILES" "" ${ARGN})
+
   # Includes Catch in the project:
   include(AddCatch)
   enable_testing(true)  # Enables unit-testing.
+
+  set(BIN_TEST ${ARG_TARGET_NAME})
+  set(TEST_FILES ${ARG_FILES})
 
   add_executable(
     # executable name
@@ -15,37 +22,20 @@ if(BUILD_TESTS)
   )
 
   set_property(TARGET ${BIN_TEST} APPEND PROPERTY INCLUDE_DIRECTORIES ${CATCH_INCLUDE_DIR})
-
   add_dependencies(${BIN_TEST} catch)
-  target_link_libraries(${BIN_TEST} ${LIB_NAME})
 
-  set(TEST_REPORT_FILE "testresults.xml")
-  set(TEST_REPORT_TARGET "testreport")
   add_test(NAME catch-tester COMMAND ${BIN_TEST})
 
-  list(APPEND testerArgs "--catchoption=-r junit --out ${TEST_REPORT_FILE}")
-
-  # Execute ${CMAKE_CURRENT_BINARY_DIR}/${TEST_EXEC} binary to execute tests
-  add_custom_command(OUTPUT ${TEST_REPORT_FILE}
-    DEPENDS ${BIN_TEST}
-    COMMAND "${CMAKE_CURRENT_BINARY_DIR}/${BIN_TEST}" ARGS ${testerArgs}
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-  add_custom_target(${TEST_REPORT_TARGET} DEPENDS ${TEST_REPORT_FILE})
-
-  if(BUILD_TESTS_COVERAGE)
+  if(ARG_COVERAGE)
     include(EnableCoverageReport)
     # --- coverage report ---
-    ENABLE_COVERAGE_REPORT(TARGETS ${BIN_TEST} FILTER "${LIB_DIR}*" TESTS ${TEST_FILES} DEPTARGETS ${TEST_REPORT_TARGET})
+    ENABLE_COVERAGE_REPORT(TARGETS ${BIN_TEST} FILTER "${CATCH_INCLUDE_DIR}*" TESTS ${TEST_FILES} DEPTARGETS "")
   endif()
 
-  if(VALGRIND_CHECK)
+  if(ARG_VALGRIND)
     include(GenerateValgrind)
     # --- Valgrind --- #
-    if (DEFINED ENV{COMMANDER_CONFIG_FILE})
-      GENERATE_VALGRIND(BIN "${CMAKE_CURRENT_BINARY_DIR}/${BIN_TEST}")
-    else()
-      GENERATE_VALGRIND(BIN "${CMAKE_CURRENT_BINARY_DIR}/${BIN_TEST}")
-    endif()
+    GENERATE_VALGRIND(BIN "${CMAKE_CURRENT_BINARY_DIR}/${BIN_TEST}")
   endif()
 
-endif()
+ENDFUNCTION()
